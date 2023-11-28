4bc02a5f9a03cb29a2efcca99a82de29ad42bc6a
jill64 <intents.turrets0h@icloud.com>
2023-11-28 14:49:10 +0900

    add fallback test

--- test/object/devalue.test.ts
+++ test/object/devalue.test.ts
@@ -19,3 +19,22 @@ test('devalue', () => {
 
   expect(() => serde.deserialize('')).toThrowError()
 })
+
+test('devalue-fallback', () => {
+  const serde = devalue(
+    (x): x is Set<Date> =>
+      !!(
+        x &&
+        typeof x === 'object' &&
+        x instanceof Set &&
+        [...x].every((x) => x instanceof Date)
+      ),
+    undefined
+  )
+
+  const check = prepare(serde)
+
+  check(new Set([new Date()]))
+
+  expect(serde.deserialize('')).toBe(undefined)
+})
--- test/object/json.test.ts
+++ test/object/json.test.ts
@@ -16,3 +16,19 @@ test('json', () => {
 
   expect(() => serde.deserialize('')).toThrowError()
 })
+
+test('json-fallback', () => {
+  const serde = json(
+    (x): x is { key: string } =>
+      !!(x && typeof x === 'object' && 'key' in x && typeof x.key === 'string'),
+    null
+  )
+
+  const check = prepare(serde)
+
+  check({
+    key: 'value'
+  })
+
+  expect(serde.deserialize('')).toBe(null)
+})
